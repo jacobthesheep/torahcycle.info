eef52cc408e0a711ab9267317932d00c59b769c8
Jacob Lamb <jacobtlamb@hey.com>
2023-05-05 11:38:50 -0700

    Add festival readings

--- src/pages/data.ts
+++ src/pages/data.ts
@@ -22,5 +22,36 @@ export const TorahReading = z.object({
 	),
 });
 
+export const FestivalReading = z.object({
+	holiday: z.object({
+		hebrew: z.string(),
+		english: z.string().optional(),
+	}),
+	month: z.number().optional(),
+	date: z.number().optional(),
+    time: z.string().optional(),
+	torah: z.array(
+		z.object({
+			book: z.string(),
+			verses: z.string(),
+		}),
+	),
+	haftorah: z.array(
+		z.object({
+			book: z.string(),
+			verses: z.string(),
+		}),
+	),
+	apostolic: z.array(
+		z.object({
+			book: z.string(),
+			verses: z.string(),
+		}),
+	),
+});
+
+export const FestivalReadings = z.array(FestivalReading);
+export type FestivalReadingType = z.infer<typeof FestivalReading>;
+
 export const TorahReadings = z.array(TorahReading);
 export type TorahReadingType = z.infer<typeof TorahReading>;
