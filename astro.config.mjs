de7eb5a7d0c39bfe91f02ccd8623eb723730de09
Jacob Lamb <jacobtlamb@hey.com>
2023-05-12 17:48:56 -0700

    Add svelte and nano-stores

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
+import svelte from "@astrojs/svelte";
 
 export default defineConfig({
-	site: "https://torahcycle-info.vercel.app/",
-	output: "server",
-	adapter: vercel(),
+  site: "https://torahcycle-info.vercel.app/",
+  output: "server",
+  adapter: vercel(),
+  integrations: [svelte()]
 });
