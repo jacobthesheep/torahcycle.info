03907698d00c2dc14063b904fa29c8f505652a32
Jacob Lamb <jacobtlamb@hey.com>
2023-05-17 14:24:04 -0700

    Replace svelte store and data with @hebcal packages

--- src/stores.ts
+++
@@ -1,6 +0,0 @@
-import { atom } from "nanostores";
-
-import type { TorahReadingType } from "../src/pages/data";
-
-export const parsedTorahReadings = atom<TorahReadingType[]>([]);
-export const error = atom<string | null>(null);
