80d9f95ac01c7f5277f5d05d6e449190320c408d
Jacob Lamb <jacobtlamb@hey.com>
2023-05-24 21:18:12 -0700

    Update site
    - Remove svelte and nanostores
    - Update page design
    - Now show the current week's events and readings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
-import svelte from "@astrojs/svelte";
 
 export default defineConfig({
   site: "https://torahcycle-info.vercel.app/",
   output: "server",
   adapter: vercel(),
-  integrations: [svelte()]
 });
